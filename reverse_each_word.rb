4b929042a6b6c24c9a3e87aa0028bc6e38d0ce40
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 20:58:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  reversed_string = " "
+  reversed_string = ""
   string.split(" ").each do |word|
-    reversed_string << "#{word.reverse}"
+    reversed_string << "#{word.reverse} "
   end
-  return reversed_string
+  return reversed_string.strip
 end
